a1902913a199e0e2b6238108006b2688fb5c9d97
Yash <79582883+yash-aryan@users.noreply.github.com>
2024-05-25 17:07:23 +0530

    Create Server to Send and Render Messages
    - Created basic express template.
    - Update index.js to contain messages array & render the messages.
    - Update index.ejs in views to iterate over and render the incoming
    messages.
    - Setup nodemon to refresh server on file changes.

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+module.exports = router;
